534be716d2d351265cafd32811256bb3ba4606ea
NindiFaradinaPutri <ninditaputri654@gmail.com>
2025-11-04 19:42:35 +0700

    mengedit riwayat terbaru

--- customer/inside/home.php
+++ customer/inside/home.php
@@ -2,10 +2,8 @@
 require '../../database/connect.php';
 require '../include/home_f.php';
 
-// ambil kode unik dari URL (misal: ?kode=abc123)
 $kode_unik = $_GET['kode'] ?? '';
 
-// ambil data meja berdasarkan kode unik
 $meja = getMejaByKode($kode_unik, $conn);
 
 if (!$meja) {
@@ -36,7 +34,7 @@ if (!$meja) {
         <div class="aksi">
             <a href="menu.php?kode=<?= urlencode($kode_unik); ?>" class="btn">Lihat Menu</a>
         </div>
-
+ 
         <div class="footer">
             <a href=""></a>
         </div>
