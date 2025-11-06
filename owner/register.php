1d0ea14ce6b10ced0130e44ffb4d8840c197144f
Kholil7 <muhammadholel2h@gmail.com>
2025-11-06 08:04:45 +0700

    memperbarui tampilan register owner

--- owner/register.php
+++ owner/register.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Register</title>
-    <link rel="stylesheet" href="../css/styleOwner.css" />
-
+    <?php $version = filemtime('../css/styleOwner.css'); ?>
+    <link rel="stylesheet" type="text/css" href="../css/styleOwner.css?v=<?php echo $version; ?>">
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
     />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   </head>
   <body>
     <div class="container">
       <div class="frame">
-        <h1>Register</h1>
+        <h1>Register Owner</h1>
         <form action="include/registerProses.php" method="POST">
           <label for="">Nama</label>
           <input type="text" name="nama" placeholder="Masukkan nama" required />
