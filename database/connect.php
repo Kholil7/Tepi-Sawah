298cba0c7c3ba615dcf2563070ff32b836fe3c96
Kholil7 <muhammadholel2h@gmail.com>
2025-11-24 06:12:57 +0700

    perbaika quary menambahkan timezone

--- database/connect.php
+++ database/connect.php
@@ -1,7 +1,7 @@
 <?php
 $host     = "localhost";
-$user     = "root"; 
-$password = ""; 
+$user     = "root";
+$password = "";
 $database = "dbresto_app";
 
 $conn = mysqli_connect($host, $user, $password, $database);
@@ -10,5 +10,7 @@ if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
 
-// echo "Koneksi berhasil";
+date_default_timezone_set('Asia/Jakarta');
+mysqli_query($conn, "SET time_zone = '+07:00'");
+
 ?>
