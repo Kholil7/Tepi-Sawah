c85aacf58ef4872f3ff91e9f1fa73abf720489d2
Kholil7 <muhammadholel2h@gmail.com>
2025-11-25 21:22:19 +0700

    testing

--- database/connect.php
+++ database/connect.php
@@ -1,8 +1,22 @@
 <?php
-$host     = "sql106.infinityfree.com";
-$user     = "if0_40493381";
-$password = "9t6CHUY4Vde5";
-$database = "if0_40493381_db_resto";
+// $host     = "sql106.infinityfree.com";
+// $user     = "if0_40493381";
+// $password = "9t6CHUY4Vde5";
+// $database = "if0_40493381_db_resto";
+
+// $conn = mysqli_connect($host, $user, $password, $database);
+
+// if (!$conn) {
+//     die("Koneksi database gagal: " . mysqli_connect_error());
+// }
+
+// date_default_timezone_set('Asia/Jakarta');
+// mysqli_query($conn, "SET time_zone = '+07:00'");
+
+$host     = "localhost";
+$user     = "root";
+$password = "";
+$database = "dbresto_app";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
